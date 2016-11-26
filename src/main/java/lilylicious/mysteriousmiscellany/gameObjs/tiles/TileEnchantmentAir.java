60de49d0d15c6384777604c07fe5f22902cfa515
Lily <lily.hammerskog@gmail.com>
2016-11-26 18:47:29 +0100

    Proper Name fix

---
+++ src/main/java/lilylicious/mysteriousmiscellany/gameObjs/tiles/TileEnchantmentAir.java
@@ -0,0 +1,12 @@
+package lilylicious.mysteriousmiscellany.gameObjs.tiles;
+
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ITickable;
+
+public class TileEnchantmentAir extends TileEntity implements ITickable {
+
+    @Override
+    public void update() {
+
+    }
+}
