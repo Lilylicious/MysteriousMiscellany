6af7273a7d5f19210cd1260d0a67eee98b53f343
Lily <lily.hammerskog@gmail.com>
2017-01-16 16:50:59 +0100

    Test tests

---
+++ src/test/java.lilylicious.mysteriousmiscellany/MMTests.java
@@ -0,0 +1,12 @@
+package java.lilylicious.mysteriousmiscellany;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MMTests {
+
+    @Test
+    public void testTest() {
+        assertTrue(1 == 1);
+    }
+}
