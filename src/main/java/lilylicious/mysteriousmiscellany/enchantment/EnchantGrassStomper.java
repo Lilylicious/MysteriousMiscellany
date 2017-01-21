91cc8c7ea6b5ada99130cb9480d8a01702c016fc
Lily <lily.hammerskog@gmail.com>
2017-01-21 01:54:25 +0100

    Fixes enchant

--- src/main/java/lilylicious/mysteriousmiscellany/enchantment/EnchantGrassStomper.java
+++ src/main/java/lilylicious/mysteriousmiscellany/enchantment/EnchantGrassStomper.java
@@ -26,16 +26,14 @@ public class EnchantGrassStomper extends EnchantmentBase {
     @SubscribeEvent
     public void onPlayerTick(TickEvent.PlayerTickEvent event) {
         EntityPlayer player = event.player;
-        ItemStack boots = null;
+        ItemStack boots = player.getItemStackFromSlot(EntityEquipmentSlot.FEET);
         Block target;
 
-        for (ItemStack stack : player.getArmorInventoryList()) {
-            if (stack != null && player.getEntityWorld().isRemote && stack.getItem() instanceof ItemArmor && ((ItemArmor) stack.getItem()).getEquipmentSlot().ordinal() == 2)
-                boots = stack;
-        }
+        if(boots == null || !(boots.getItem() instanceof ItemArmor))
+            return;
 
 
-        if (boots != null && EnchantmentHelper.getMaxEnchantmentLevel(this, player) > 0) {
+        if (!player.getEntityWorld().isRemote && EnchantmentHelper.getMaxEnchantmentLevel(this, player) > 0) {
             Iterable<BlockPos> blocks = WorldHelper.findBox(player.getPosition(), 3);
 
             for (BlockPos blockPos : blocks) {
