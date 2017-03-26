f7c6fae913522b9df6ffc210feb01e47ea980008
Lily <lily.hammerskog@gmail.com>
2017-03-26 09:39:16 +0200

    Versioning

--- src/main/java/lilylicious/mysteriousmiscellany/MMCore.java
+++ src/main/java/lilylicious/mysteriousmiscellany/MMCore.java
@@ -28,7 +28,7 @@ public class MMCore {
 
     public static final String MODID = "mysteriousmiscellany";
     public static final String MODNAME = "Mysterious Miscellany";
-    public static final String VERSION = "1.11.2-1.1.3";
+    public static final String VERSION = "1.11.2-1.1.4";
 
     @Mod.Instance(MODID)
     public static MMCore instance;
